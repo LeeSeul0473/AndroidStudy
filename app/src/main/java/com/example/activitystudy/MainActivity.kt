828193458d6d22407ce16488df01a3414f3c4d25
LeeSeul <leeseul2021@duksung.ac.kr>
2025-01-30 17:03:52 +0900

    액티비티 전환 실습

--- app/src/main/java/com/example/activitystudy/MainActivity.kt
+++ app/src/main/java/com/example/activitystudy/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.activitystudy
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -14,7 +16,17 @@ class MainActivity : AppCompatActivity() {
 /*        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets*/
+            insets
+        }*/
+        settingButton()
+    }
+
+    fun settingButton() {
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener{
+            // move to subactivity
+            val intent = Intent(this, SubActivity::class.java)
+            startActivity(intent)
         }
     }
 }
