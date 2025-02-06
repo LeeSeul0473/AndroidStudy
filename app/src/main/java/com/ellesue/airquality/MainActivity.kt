3b32fd51d2ddeaecd41ff583d4adf7614ad78170
LeeSeul <leeseul2021@duksung.ac.kr>
2025-02-06 23:10:07 +0900

    위치 가져오는 코드 구현

--- app/src/main/java/com/ellesue/airquality/MainActivity.kt
+++ app/src/main/java/com/ellesue/airquality/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.ellesue.airquality
 
 import android.Manifest
+import android.app.Activity
+import android.content.DialogInterface
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.location.LocationManager
 import android.os.Bundle
+import android.provider.Settings
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
@@ -52,7 +57,35 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun showDialogForLocationServiceSetting() {
-        TODO("Not yet implemented")
+        getGPSPermissionLauncher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ){
+            result->
+            if(result.resultCode == Activity.RESULT_OK){
+                if(isLocationServicesAvailable()){
+                    isRunTimePermissionsGranted()
+                }else{
+                    Toast.makeText(this, "위치서비스를 사용할 수 없습니다.", Toast.LENGTH_SHORT).show()
+                    finish()
+                }
+            }
+        }
+
+        val builder : AlertDialog.Builder = AlertDialog.Builder(this@MainActivity)
+        builder.setTitle("위치 서비스 비활성화")
+        builder.setMessage("위치 서비스가 비활성화 되어있습니다. 위치를 활성화해주세요.")
+        builder.setCancelable(true)
+        builder.setPositiveButton("설정", DialogInterface.OnClickListener{dialogInterface, i ->
+            val callGPSSettingIntent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+            getGPSPermissionLauncher.launch(callGPSSettingIntent)
+        })
+        builder.setNegativeButton("취소", DialogInterface.OnClickListener{dialogInterface, i ->
+            val callGPSSettingIntent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+            dialogInterface.cancel()
+            Toast.makeText(this, "위치서비스를 사용할 수 없습니다.", Toast.LENGTH_SHORT).show()
+            finish()
+        })
+        builder.create().show()
     }
 
     private fun isLocationServicesAvailable(): Boolean {
